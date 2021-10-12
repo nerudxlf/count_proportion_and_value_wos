12e6e3cf654f37d206bad2a4b27a7bad85ac94a1
nerudxlf <nerudxlf@gmail.com>
2021-10-12 10:37:13 +0600

    add plot funtion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from pandas import DataFrame
+from mpldatacursor import datacursor
 
 
 def count(data: DataFrame, field: str) -> DataFrame:
@@ -24,10 +26,25 @@ def count(data: DataFrame, field: str) -> DataFrame:
     return result_df
 
 
+def plot_info(data: DataFrame) -> None:
+    x = data["Values"].to_list()
+    y = data["Category Normalized Citation Impact"].to_list()
+    name = data["Research Area"].to_list()
+    plt.plot()
+    ax = plt.gca()
+    for i in range(len(data)):
+        ax.scatter(x[i], y[i], label=name[i])
+    datacursor(formatter='{label}'.format)
+    plt.plot([0, max(x)], [1, 1])
+    plt.plot([100, 100], [0, max(y)])
+    plt.show()
+
+
 def main():
     data_df_wos: DataFrame = pd.read_excel("data/WoS Research Areas 2010-2020.xlsx")
     data_df_wos_ar: DataFrame = pd.read_excel("data/WoS Research Areas 2010-2020 AR.xlsx")
     result_df_wos: DataFrame = count(data_df_wos, "Category Normalized Citation Impact")
     result_df_wos_ar: DataFrame = count(data_df_wos_ar, "Category Normalized Citation Impact")
+    plot_info(result_df_wos_ar)
     result_df_wos.to_excel("WoS Research Areas 2010-2020 result.xlsx", index=False)
     result_df_wos_ar.to_excel("WoS Research Areas 2010-2020 AR result.xlsx", index=False)
